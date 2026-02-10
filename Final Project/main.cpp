47992584dc120f06b19731e215f6cfd38045858d
Frank Colorado <chawnkiee@gmail.com>
2026-02-09 21:25:26 -0800

    added character creation to main function

--- Final Project/main.cpp
+++ Final Project/main.cpp
@@ -16,5 +16,14 @@ struct Player {
 
 int main() {
 
+	cout << "Welcome to this simple DnD like game!" << endl;
+	cout << "Please enter the name of your character: ";
+	string playerName;
+	cin >> playerName;
+
+	Player player(playerName, 100, 20);
+
+	cout << "\nYou have entered the world as " << player.name << " with " << player.hp << " HP and " << player.atkPwr << " attack power." << endl;
+
 	return 0;
 }
